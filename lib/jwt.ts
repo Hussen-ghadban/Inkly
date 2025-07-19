e15b6f4fc96983d61965d8ad24128ffce245e7c6
Hussen-ghadban <houssein.ghadban99@gmail.com>
2025-07-19 18:39:05 +0300

    add crud for posts

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,15 +1,21 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "dev-secret"; // 🔐 Use a strong secret in prod
+const JWT_SECRET = process.env.JWT_SECRET|| "default_secret_key";
+export interface DecodedToken {
+  id: string;
+  email: string;
+  // add more fields if needed
+}
+
 
 export function signJwtToken(payload: object) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
 }
 
-export function verifyJwtToken(token: string) {
+export function verifyJwtToken(token: string): DecodedToken | null {
   try {
-    return jwt.verify(token, JWT_SECRET);
-  } catch {
+    return jwt.verify(token, process.env.JWT_SECRET!) as DecodedToken;
+  } catch  {
     return null;
   }
 }
